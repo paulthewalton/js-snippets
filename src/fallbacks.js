6fedc1e145512f8ec0e8f6b650dd2aa0f64ba831
Paul Walton <paul@denmandigital.com>
2020-07-17 15:19:51 -0700

    fallback returns undefined if no values passed

--- src/fallbacks.js
+++ src/fallbacks.js
@@ -1,11 +1,20 @@
+/**
+ * @file
+ * @kind overview
+ * @summary Utilities for fallback and default values.
+ * @author Paul Walton
+ */
+
 import { isCallable } from "./type";
 
 /**
- * Return the first argument that satisfies a validation callback, or the last
- * value provided.
+ * Return the first argument that satisfies a validation callback, or the last value provided.
+ * * Returns `undefined` if no values are passed.
+ * @summary Return the first argument that satisfies a validation callback.
  * @arg {Function} validate - Validation callback.
  * @arg {...*} values - Variable number of candidate values.
- * @returns {*}
+ * @throws {TypeError} If the callback isn't callable.
+ * @returns {?*}
  */
 export function fallbackUntil(validate, ...values) {
 	if (!isCallable(validate)) {
@@ -16,5 +25,5 @@ export function fallbackUntil(validate, ...values) {
 			return value;
 		}
 	}
-	return values.length ? values.pop() : null;
+	return values.length ? values.pop() : undefined;
 }
