52dfd57a8ba7f6480f02bb4e5b925d9dd7ffcda7
Paul Walton <paul@denmandigital.com>
2020-07-17 15:20:30 -0700

    moved compareProps from objects to comparisons

--- src/comparisons.js
+++ src/comparisons.js
@@ -12,3 +12,20 @@ export function byNumericValue(a, b) {
 			return a - b;
 	}
 }
+
+/**
+ * Compare a property of 2 objects.
+ * @arg {string} prop The property by which to evaluate.
+ * @arg {Object} subjectA The object to evaluate.
+ * @arg {Object} subjectB Another object to evaluate.
+ * @returns {number} 0 if equal, 1 if more, -1 if less.
+ */
+export function compareProps(prop, subjectA, subjectB) {
+	if (subjectA[prop] < subjectB[prop]) {
+		return -1;
+	}
+	if (subjectA[prop] > subjectB[prop]) {
+		return 1;
+	}
+	return 0;
+}
--- src/objects.js
+++ src/objects.js
@@ -33,23 +33,6 @@ export function matchProps(prop, subjectA, subjectB) {
 }
 
 /**
- * Compare a property of 2 objects.
- * @arg {string} prop The property by which to evaluate.
- * @arg {Object} subjectA The object to evaluate.
- * @arg {Object} subjectB Another object to evaluate.
- * @returns {number} 0 if equal, 1 if more, -1 if less.
- */
-export function compareProps(prop, subjectA, subjectB) {
-	if (subjectA[prop] < subjectB[prop]) {
-		return -1;
-	}
-	if (subjectA[prop] > subjectB[prop]) {
-		return 1;
-	}
-	return 0;
-}
-
-/**
  * Copy properties from one object to another recursively.
  * @arg {Object} target Target object.
  * @arg {Object} extensions Object with properties to copy.
